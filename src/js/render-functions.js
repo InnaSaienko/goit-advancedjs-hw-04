c6bc9f481005566ac4400c6482cdcbb4438286b5
Inna Saienko <inna.saienko.nl@gmail.com>
2026-06-17 09:28:12 +0200

    feat: remove commented import line

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,7 +2,6 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// import { loadMoreBtn } from '../main.js';
 
 //Show message form according state
 const messages = {
